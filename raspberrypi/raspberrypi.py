d9feef69bcbb9a84a2db5ef184f140a01f3346e9
include07 <j.elfirqi@gmail.com>
2025-05-06 23:37:54 +0100

    modify frequency

--- raspberrypi/raspberrypi.py
+++ raspberrypi/raspberrypi.py
@@ -19,7 +19,7 @@ PIN_LEFT_MOTORS_LPWM = 23
 PIN_LEFT_MOTORS_RPWM = 24
 
 # PWM Parameters
-PWM_FREQUENCY = 100  # Hz
+PWM_FREQUENCY = 8000  # Hz
 DEFAULT_SPEED = 20   # Duty cycle percentage (0-100) for movement
 
 # --- Global PWM Objects ---
